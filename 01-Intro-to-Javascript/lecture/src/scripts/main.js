976626286ef952ccc21e6d65bc83aecff1f5374f
mariblan <mariablancocanal@gmail.com>
2026-07-16 10:29:38 +0200

    Added high order array methods

--- 01-Intro-to-Javascript/lecture/src/scripts/main.js
+++ 01-Intro-to-Javascript/lecture/src/scripts/main.js
@@ -1,115 +1,96 @@
-// Arrays
+function greet(name) {
+  return `Hello, ${name}!`;
+}
 
-const numbers = [0, 1, 2, 3, 4, 5, 6]; 
-const strings = ['one', 'two', 'three', 'four', 'five'];
-// numbers = 33
+function goodMorning(name) {
+  return `Good Morning, ${name}`
+}
+// High order functions
+function greetUser(name, callback) {
+  return callback(name); // callback function
+}
 
-// console.log(numbers);
-// console.log(numbers[0]);
-// console.log(numbers[6]);
-// console.log(numbers.length);
-// console.log(numbers[numbers.length - 1])
-// console.log(numbers[numbers.length / 2])
+console.log(greetUser("Harry", greet))
+console.log(greetUser("Harry", goodMorning))
 
-// push and pop - work at the last index
-// console.log(strings)
+const numbers = [1, 2, 3, 20, 5, 15];
 
-// const removedItem = strings.pop()
-// console.log(strings)
-// console.log(removedItem)
-
-// strings.push("eight");
-// console.log(strings)
-
-// // shift and unshift - they work always at index 0
-// strings.shift()
-// console.log(strings)
-
-// strings.unshift("9")
-// console.log(strings)
-
-// reverse() and toReversed()
-// numbers.reverse()
-// console.log(numbers)
-
-// const reversedNumbers = numbers.toReversed()
-// console.log(numbers)
-// console.log(reversedNumbers)
-
-// splice and toSpliced
-// numbers.splice(4, 0, "3", "2", "4")
-// numbers.splice(4)
-// numbers.splice(4, 0, "4")
-// numbers.splice(4, 2, "4")
-// console.log(numbers)
-
-// console.log(numbers);
-// const splicedArray = numbers.toSpliced(4, 2, '4');
-// console.log(numbers)
-// console.log(splicedArray)
-
-// const slicedNumbers = numbers.slice(3)
-const slicedNumbers = numbers.slice(4)
-// console.log(slicedNumbers);
-const locations = [ "Berlin", "Tokyo", "London", "Madrid"];
-locations[2] = "Brasil";
-// console.log(locations)
-// const topLocations = locations.slice(0, 3);
-const topLocations = locations.slice(-3);
-// console.log(topLocations)
-
-// console.log(locations.join(" - "))
-
-const student = {
-  name: 'Ana',
-  age: 25,
-  city: 'Barcelona',
-};
-
-// console.log(student)
-// console.log(student.name)
-// console.log(student.city)
-// console.log(student["name"])
-
-student.age = 26;
-// console.log(student.age)
-
-student.email = "ana@gmail.com";
-// console.log(student)
-
-
-const students = [
-  {
-    name: 'Ana',
-    age: 25,
-    hobbies: ["singing", "cooking"],
-    address: {
-      street: "Gartenstr.",
-      number: 22,
-      postalCode: 24422,
-      city: "Barcelona"
-    }
-  },
-  {
-    name: 'Paul',
-    age: 30,
-    city: 'Madrid',
-  },
-  {
-    name: 'John',
-    age: 35,
-    city: 'Valencia',
+function logItem(item) {
+  console.log(item)
+}
+function stringifyItems(item) {
+  if(typeof item !== "string") {
+    console.log(item.toString())
   }
-];
+}
+function increaseByOne(item) {
+  return item + 1
+}
+function mapAllItems(array, callback) {
+  for(const item of array) {
+    const newValue = callback(item);
+    
+  }
+}
+// mapAllItems(numbers, logItem);
+// mapAllItems(numbers, stringifyItems)
 
-console.log(students)
-console.log(students[0])
-console.log(students[0].name)
-console.log(students[0].hobbies)
-console.log(students[0].hobbies[1])
-console.log(students[0].address.city)
+const students = ["Harry", "Tom", "Jane"]
 
-// const arrayOfArrays = [[1,2],[2,3]]
-// console.log(arrayOfArrays)
-// console.log(arrayOfArrays[0])
-// console.log(arrayOfArrays[0][1])
+let studentString = "";
+const newArray = [];
+
+// forEach - return undefined
+students.forEach((student) => studentString += `${student}, `);
+const forEachVariable = students.forEach((student) => newArray.push(student));
+console.log(forEachVariable)
+console.log(studentString)
+console.log(newArray)
+
+// map - returns a new array
+
+const increase1 = numbers.map((number) => number + 1);
+console.log(increase1);
+const greetStudents = students.map((student) => `Hello, ${student}`);
+console.log(greetStudents)
+
+// filter - return a new array
+const numbersBiggerThan10 = numbers.filter((number) => number > 10)
+console.log(numbersBiggerThan10)
+
+// find - return one item of the array
+const numberBiggerThan5 = numbers.find((number) => number > 5);
+console.log(numberBiggerThan5)
+
+// reduce - 
+const totalSumOfNumbers = numbers.reduce((total, number) => {
+  return total + number
+}, 0)
+console.log(totalSumOfNumbers)
+
+const studentsString = students.reduce((accumulator, currentStudent) => {
+  let currentIndex = students.indexOf(currentStudent);
+  if(currentIndex === students.length - 1) {
+    return (accumulator += `${currentStudent}`);
+  } else {
+    return (accumulator += `${currentStudent}, `);
+  }
+}, "");
+console.log(studentsString);
+
+// some - boolean
+const hasItemsBiggerThan10 = numbers.some((number) => number > 19);
+console.log(hasItemsBiggerThan10)
+
+// every - boolean
+const allItemsBiggerThan10 = numbers.every((number) => number > 10);
+console.log(allItemsBiggerThan10);
+console.log(numbers)
+
+// sort
+// numbers.sort()
+numbers.sort((a, b) => b - a);
+console.log(numbers)
+
+students.sort()
+console.log(students);
