1778d38f9087d7be6f6e2c5b243104bf7293028b
Reagan Sasan <reagansasan@Reagans-MacBook-Pro.local>
2026-08-19 10:19:08 +0200

    controlled components + DOM events with types

--- 07-TS-in-react/react-ts/src/App.tsx
+++ 07-TS-in-react/react-ts/src/App.tsx
@@ -2,7 +2,7 @@ import Button from "./components/Button";
 import DeleteButton from "./components/DeleteButton";
 import Greeting from "./components/Greeting";
 import Card from "./components/Card";
-import { useState } from "react";
+import React, { useState } from "react";
 
 type User = {
   name: string;
@@ -14,12 +14,47 @@ export default function App() {
   // const [name, setName] = useState(""); // inferred as string
   // const [isOpen, setIsOpen] = useState(false); // inferred as boolean
 
-  const [users, setUsers] = useState<User[]>([]);
+  type Size = "sm" | "md" | "lg";
 
+  const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState("");
+  const [size, setSize] = useState<Size>("md");
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setName(e.target.value);
+  // };
+
+  const handleDropdownChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    if (value === "sm" || value === "md" || value === "lg") {
+      setSize(value);
+    }
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("clicked", e.currentTarget);
+  };
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
   return (
     <div className="flex flex-col items-start gap-8 p-8">
       <h1 className="text-3xl font-bold">React + TypeScript</h1>
-      <Greeting name="Bob" age={30} isVip={true} />
+      <select value={size} onChange={handleDropdownChange}>
+        <option value="sm">Small</option>
+        <option value="md">Medium</option>
+        <option value="lg">Large</option>
+      </select>
+      {/* <input
+        className="input"
+        placeholder="Type something"
+        type="text"
+        value={name}
+        onChange={handleChange}
+      /> */}
+      {/* <Greeting name="Bob" age={30} isVip={true} />
       <Greeting name="Mike" age={40} isVip={false} />
       <Button label="Click me" variant="secondary" />
       <DeleteButton
@@ -35,7 +70,7 @@ export default function App() {
 
       <Card>
         <Greeting name="Mike" age={40} isVip={false} />
-      </Card>
+      </Card> */}
     </div>
   );
 }
